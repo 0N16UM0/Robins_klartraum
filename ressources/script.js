f9d2cb59739a188eea8e292c620cc8dc50d4a5ce
Ishikawa-battousai <ajomdd@gmail.com>
2025-10-18 12:36:53 +0200

    mehr barrierefreiheit

--- ressources/script.js
+++ ressources/script.js
@@ -5,17 +5,14 @@ for (let i = 0; i < starCount; i++) {
   const star = document.createElement('div');
   star.classList.add('star');
 
-  // zufällige Position
   star.style.top = Math.random() * 100 + '%';
   star.style.left = Math.random() * 100 + '%';
 
-  // zufällige Größe
-  const size = Math.random() * 2 + 1; // 1–3px
+  const size = Math.random() * 2 + 1;
   star.style.width = `${size}px`;
   star.style.height = `${size}px`;
 
-  // leicht unterschiedliche Funkelgeschwindigkeit
-  const duration = 2 + Math.random() * 3; // 2–5s
+  const duration = 2 + Math.random() * 3;
   star.style.animationDuration = `${duration}s`;
 
   container.appendChild(star);
